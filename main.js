f6ad1bc32134959de4be9b2701bfe0c86862350f
Siebe <r0834360@student.thomasmore.be>
2023-05-31 10:44:25 +0200

    feat: add basic box with material

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 import * as THREE from 'three';
 
+//Scene
 const scene = new THREE.Scene();
+
+//Camera
 const camera = new THREE.PerspectiveCamera( 30, (window.innerWidth/2) / window.innerHeight, 0.1, 1000 );
 
+//Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor("pink");
 renderer.setSize( window.innerWidth/2, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
 
-// Make Canvas Responsive
+//Make Canvas Responsive
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth/2, window.innerHeight);
     camera.aspect = (window.innerWidth/2) / window.innerHeight;
@@ -28,15 +32,13 @@ const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.4);
 scene.add(ambientLight);
 
 
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-//position
-cube.rotation.x = 1.2;
-cube.rotation.y = 1.2
-;
-scene.add( cube );
+// Create Box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
+const boxMesh = new THREE.Mesh(boxGeometry, 
+boxMaterial);
+boxMesh.rotation.set(40, 0, 40);
+scene.add(boxMesh);
 
 camera.position.z = 5;
 
