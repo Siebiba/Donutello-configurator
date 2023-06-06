ce47fe2db851b572ecff2639284df1c043ee5638
Siebe <r0834360@student.thomasmore.be>
2023-06-06 22:12:17 +0200

    refactor: add map structure

---
+++ scripts/login.js
@@ -0,0 +1,40 @@
+var btnSignups = document.querySelector('.signup button').addEventListener('click', function () {
+    console.log('signup clicked');
+    let username = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    fetch('https://donutello-api.onrender.com/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    }).then(response => response.json()).then(json =>{
+        console.log(json);
+
+  
+        if (json.status == "success") {
+
+          
+            let feedback = document.querySelector('.alert');
+            feedback.textContent = "Signup success";
+            feedback.classList.remove('hidden');
+
+            let token = json.data.token;
+            localStorage.setItem('token', token);
+            window.location.href = "app.html";
+          
+        
+        }
+        else {
+            let feedback = document.querySelector('.alert');
+            feedback.textContent = "Signup failed";
+            feedback.classList.remove('hidden');
+        }
+    });
+
+});
+
